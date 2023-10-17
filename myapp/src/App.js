3c9bb7d86a055b86eaf12a91102cd452038cc1d9
aziz boussaid <azizboussaied062@gmail.com>
2023-10-17 15:08:08 +0100

    react

---
+++ myapp/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import NavBar from './NavBar';
+import Card from './Cards';
+import Carte from './Cards2'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+      <NavBar/>
+      <Card/>
+      <Carte/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
